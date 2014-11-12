bcd2ee7b354b4f2ec8f61947d711af32eafb8623
Steph Reaves <null>
2014-11-12 11:57:25 -0500

    login, signup, authentication routes and views, validations on user, and back end error handling of authentication

---
+++ app/controllers/auth.rb
@@ -0,0 +1,47 @@
+get '/' do
+  erb :'index'
+end
+
+get '/login' do
+  erb :'/auth/login'
+end
+
+post  '/login' do
+  user = User.find_by(username: params[:user][:username]).try(:authenticate, params[:user][:password])
+
+  if user
+    session[:user_id] = user.id
+    redirect '/'
+  else
+    set_error "Please check your password and username"
+    redirect '/login'
+  end
+end
+
+post '/login' do
+
+  redirect'/'
+end
+
+get '/signup' do
+  erb :'/auth/signup'
+end
+
+post '/signup' do
+  user = User.new(params[:user])
+
+  if user.save
+    session[:user_id] = user.id
+    redirect '/'
+  else
+    set_error "Pick another name, password, or valid email"
+    redirect "/signup?name=#{params[:user][:username]}"
+  end
+
+end
+
+get '/logout' do
+  session[:user_id] = nil
+  redirect '/'
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,13 @@
 class User < ActiveRecord::Base
   # Remember to create a migration!
+  has_secure_password
+
+  validates :username, uniqueness: true
+  validates :email, uniqueness: true
+
   has_many :campaigns
   has_many :comments
 
   has_many :votes, as: :votable
 
-  has_secure_password
 end
